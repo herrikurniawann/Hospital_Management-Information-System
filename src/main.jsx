389efaf346177874533f8b3c276f03f2f4b6804e
HeRku <herrikurniawan211@gmail.com>
2025-09-13 12:40:16 +0800

    Revert "add login page with integrate supabase"
    This reverts commit aaea3d4c89ed69a5d852eaf59a9d05d20d461d57.

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,10 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter, Route, Routes} from "react-router-dom";
-import Layout from "./components/layout";
-import UserLogin from "./components/userLogin";
-import "./input.css";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './input.css'
+import App from './App.jsx'
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route element={<Layout/>}>
-          <Route path="/login" element={<UserLogin/>} />
-        </Route>
-        <Route path="/main">
-          
-        </Route>
-      </Routes>
-    </BrowserRouter>
-    <BrowserRouter />
-  </StrictMode>
-);
+    <App />
+  </StrictMode>,
+)
